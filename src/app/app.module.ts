772fa956342bb2a6c719d44660f6ad291af55805
Anurag <v.anuragsharma@gmail.com>
2023-02-18 09:03:20 -0600

    Added header and body

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AngularMaterialModule } from "./angular-material.module"
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AppToolbarComponent } from './app-toolbar/app-toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppToolbarComponent
   ],
   imports: [
     BrowserModule,
+    AngularMaterialModule,
     AppRoutingModule
   ],
   providers: [],
