bbf1d28c19c6d7980320da7dfc734111d9458aad
Anurag <v.anuragsharma@gmail.com>
2023-04-19 01:07:10 -0500

    Update to code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ApFlexGridCardsComponent } from './ap-ui/ap-flex-grid-cards/ap-flex-grid-cards.component';
-import { ApHomeComponent } from './ap-ui/ap-home/ap-home.component';
 
-const routes: Routes = [
-  { path: 'ap-flex-grid-cards', component: ApFlexGridCardsComponent },
-  { path: 'home', component: ApHomeComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }, // redirect to `home`
-];
+const routes: Routes = [ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,24 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { Router } from "@angular/router";
+
+import { AgGridModule } from 'ag-grid-angular';
 
 import { AngularMaterialModule } from "./angular-material.module"
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ApCardComponent } from './ap-ui-utils/ap-card/ap-card.component';
-import { ApToolbarComponent } from './ap-features/ap-toolbar/ap-toolbar.component';
-import { ApHomeComponent } from './ap-ui/ap-home/ap-home.component';
-import { ApSiteSettingsComponent } from './ap-ui/ap-site-settings/ap-site-settings.component';
-import { Router } from "@angular/router";
-import { ApFlexGridCardsComponent } from './ap-ui/ap-flex-grid-cards/ap-flex-grid-cards.component';
+import { ApToolbarComponent } from "./ap-toolbar/ap-toolbar.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ApCardComponent,
     ApToolbarComponent,
-    ApHomeComponent,
-    ApSiteSettingsComponent,
-    ApFlexGridCardsComponent
   ],
   imports: [
     BrowserModule,
     AngularMaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
